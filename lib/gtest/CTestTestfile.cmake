2fd5583c3d5f06f910bc546b5eed13c1ca83813c
Kyle Piddington <kpiddy@gmail.com>
2015-12-02 10:30:33 -0800

    Pushing with libraries included

---
+++ lib/gtest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /Users/kpidding/Documents/git/SharkProject/lib/gtest
+# Build directory: /Users/kpidding/Documents/git/SharkProject/lib/gtest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
